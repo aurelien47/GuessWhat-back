97d00717fde8f2f9f2d6f390324a7da7fad4a392
Nicolas <nicolas.magassa@yahoo.com>
2024-01-22 10:34:11 +0100

    banch nico-back

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -14,7 +14,7 @@ const userController = {
         email,
         password
       } = req.body;
-      // une fois toute la vérif formulaire passé et validé on peut inscrire l'utilisateur
+      // une fois toute la vérif formulaire passé et validé on peut inscrire (enregistrer) l'utilisateur en BDD
       // mais avant on chiffre le MDP
       encryptedPassword = await bcrypt.hash(password, 10);
 
