25deeaff08a3872a6232e6fd3b77b35b182c379f
Nicolas <nicolas.magassa@yahoo.com>
2024-01-24 17:40:47 +0100

    refactiring

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -6,8 +6,9 @@ const themeAlreadyExist = require('./themeAlreadyExist.middleware');
 const verifyBodyRiddle = require('./riddleAlreadyExist.middleware');
 const riddleAlreadyExist = require('./riddleAlreadyExist.middleware');
 const verifyDuplicate = require('./verifyDuplicate.middleware');
+const verifyUserProfile = require('./verifyUserProfile.middleware');
 
 
 
-module.exports = {isAdmin, verifyJwt, userAlreadyExist, verifyBodyRegister, themeAlreadyExist, verifyBodyRiddle, riddleAlreadyExist, verifyDuplicate};
+module.exports = {isAdmin, verifyJwt, userAlreadyExist, verifyBodyRegister, themeAlreadyExist, verifyBodyRiddle, riddleAlreadyExist, verifyDuplicate, verifyUserProfile};
 
--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -1,17 +1,17 @@
 const express = require('express'); // on importe express                              
 const router = express.Router(); // creation routeur avec express.Router() 
 //const userProfile = require('../controllers/profile.controller'); // on importe userprofile controller
-const verifyProfile = require('../middlewares/verifyUserProfile'); // on importe userProfile middleware
+const { verifyUserProfile} = require('../middlewares'); // on importe userProfile middleware
 const{ getUserProfile, modifyOneUserProfile, deleteOneUserProfile } = require('../controllers/profile.controller'); // on importe getUserProfile controller
 
 
-router.get('/profile/:id', verifyProfile, getUserProfile); // verifyJWT sera exécuté en premier pour s'assurer que l'utilisateur 
+router.get('/profile/:id', verifyUserProfile, getUserProfile); // verifyJWT sera exécuté en premier pour s'assurer que l'utilisateur 
 //est authentifié avant de permettre l'accès à modifyOneUserProfile  
 
-router.put('/profile/:id', verifyProfile, modifyOneUserProfile); // route pour que l'user modifie son profile
+router.put('/profile/:id', verifyUserProfile, modifyOneUserProfile); // route pour que l'user modifie son profile
 // execution de verifyJWT pour l'authentification et userProfileMiddleware pour s'assurer que l'utilisateur ne modifie que son propre profil.
 
-router.delete('/profile/:id', verifyProfile, deleteOneUserProfile); // route pour que l'user supprime son profile
+router.delete('/profile/:id', verifyUserProfile, deleteOneUserProfile); // route pour que l'user supprime son profile
 
 module.exports = router; // on exporte le router
 
