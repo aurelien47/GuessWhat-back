9cb37d42e79284ad4510f11f787ac94323f3cbdf
aurelien47 <cambou.aurelien@gmail.com>
2024-01-26 09:33:03 +0100

    vérif profil done

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,5 +1,6 @@
 const { User } = require ('../models');  
 const bcrypt = require('bcrypt'); 
+const { Op } = require('sequelize');
 
 const profileController = {             
 
@@ -24,16 +25,61 @@ const profileController = {
     async modifyOneUserProfile (req, res) {
         try {
             const user = await User.findByPk(req.params.id, {
-                attributes: ["password","id"]
+                attributes: ["password","id", "username", "email"]
             }); 
+            
             if (!user) {
                 return res.status(404).send('Utilisateur non trouvé');
             }
             console.log('user', user.password)
+
             // Mettre à jour l'utilisateur avec req.body
             // Assurez-vous de valider et de nettoyer req.body avant de l'utiliser
+            
             let updatedData = req.body;
 
+
+            // Pseudo 
+            if(updatedData.username) {
+                console.log('updatedData', updatedData);
+                // Vérifier si le nouveau pseudo est identique aux données actuelles de l'utilisateur
+                if (user.username === updatedData.username) {
+                    return res.status(400).send("Vous avez saisi le même pseudo qu'auparavant.");
+                }
+    
+                // Vérifier si le nouveau pseudo existe déjà pour un autre utilisateur
+                const existUsername = await User.findOne({
+                    where: {
+                        username: updatedData.username,
+                        id: {[Op.not]: req.params.id } // Exclure l'utilisateur actuel de la recherche
+                    }
+                });
+
+                if (existUsername) {
+                    return res.status(400).send("Ce pseudo est déjà utilisé par un autre utilisateur.");
+                }
+            }
+            
+            // Email
+            if(updatedData.email) {
+                console.log('updatedData', updatedData);
+                // Vérifier le nouvel email est identique aux données actuelles de l'utilisateur
+                if (user.email === updatedData.email) {
+                    return res.status(400).send("Vous avez saisi le même mail qu'auparavant.");
+                }
+
+                // Vérifier si le nouvel email existe déjà pour un autre utilisateur
+                const existEmail = await User.findOne({
+                    where: {
+                        username: updatedData.email,
+                        id: {[Op.not]: req.params.id } // Exclure l'utilisateur actuel de la recherche
+                    }
+                });
+
+                if (existEmail) {
+                    return res.status(400).send("Cet email est déjà utilisé par un autre utilisateur.");
+                }
+            }
             
             // Prévoir le cas d'une modificiation de mot de passe
             if(updatedData.password) {
@@ -50,11 +96,9 @@ const profileController = {
             await user.save();
             res.status(201).json({status: 'success'});
 
-            
         } catch (error) {
             console.error(error);
             res.status(500).send('Erreur lors de la modification du profil');
-
         }
     },
 
@@ -74,8 +118,6 @@ const profileController = {
             res.status(500).send('Erreur lors de la suppression du profil');
         }
     },
-
-
 };  
 
 
@@ -85,7 +127,3 @@ const profileController = {
 
 
 module.exports = profileController;
-
-
-
-
